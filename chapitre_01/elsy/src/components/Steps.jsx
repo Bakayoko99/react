a699957203da0dea020046f03491ffe41c4d3dbb
unknown <you@example.com>
2021-04-07 16:50:05 +0200

    elsy: 03 composants not finished

---
+++ chapitre_01/elsy/src/components/Steps.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+class Steps extends React.Component{
+    render() {
+        return(
+            <div className="box col-sm-3 col-6">
+                <span style="fontSize = 100" style="color = black" class="material-icons">directions_walk</span>
+            </div>
+        );
+    }
+}
+
+export default Steps;
