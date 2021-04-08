1d81c4b7878a4e4475a4234cf5f3b9e5fe3ec006
unknown <you@example.com>
2021-04-08 17:34:35 +0200

    teams debut

---
+++ chapitre_01/teams/src/App.js
@@ -0,0 +1,16 @@
+
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Liste d'equipes de foot d'angleterre</h1>
+      <h3>Team is: Arsenal and its color is: red</h3>
+      <p>Amount of points is:</p>
+      <p>
+      </p>
+    </div>
+  );
+}
+
+export default App;
