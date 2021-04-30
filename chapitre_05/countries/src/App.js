960a03778031ad7dc44cadbd6a056528dfc500fe
Bakagnan <cr7sendu@gmail.com>
2021-04-30 12:33:41 +0200

    countries exo styles

--- chapitre_05/countries/src/App.js
+++ chapitre_05/countries/src/App.js
@@ -43,17 +43,18 @@ class App extends React.Component {
       })
 
   }
-  
-  getCountry(country) {
-    console.log("getcountry essay: ",this.state.input);
-    fetch("https://restcountries.eu/rest/v2/name/" + this.state.input)
+
+  getCountry() {
+    console.log("getcountry essay: ", this.state.input);
+    fetch("http://localhost:8000/country/" + this.state.input)
       .then(response => response.json())
       .then(result => {
-        const countrieName = result[0].name;
-        const countrieCap = result[0].capital;
-        const countrieFlag = result[0].flag;
-        const countriePopul = result[0].population;
-        const countrieRegion = result[0].region;
+        console.log(result.country);
+        const countrieName = result.country.name;
+        const countrieCap = result.country.capital;
+        const countrieFlag = result.country.flag;
+        const countriePopul = result.country.population;
+        const countrieRegion = result.country.region;
 
 
         this.setState({
@@ -68,7 +69,7 @@ class App extends React.Component {
       })
   }
 
-  handleUsername(e){
+  handleCountryName(e) {
     this.setState({
       input: e.target.value
 
@@ -80,15 +81,28 @@ class App extends React.Component {
     console.log("render");
     console.log("input: ", this.state.input);
     return (
+      
+      <div className="container">
+        <div className="row">
 
-      <div>
+          <div className="col-8 offset-4">
+            <h1>Country selector</h1>
+          </div>
 
-        <input class="form-control" type="text" placeholder="Enter country" onChange={(e)=> this.handleUsername(e)} ></input>
-        <Button clickButton={() => this.getCountry("france")} children="France"></Button>
-        <Button clickButton={() => this.getCountry("brazil")} children="Brazil"></Button>
-        <Button clickButton={() => this.getCountry("croatia")} children="Croatia"></Button>
-        <Card countryImg={this.state.flag} countryName={this.state.name} countryCap={this.state.capital} countryReg={this.state.region} countryPop={this.state.population}></Card>
+        </div>
+        <div className="row">
+          <div className="col-6 offset-3">
+            <p>Country</p>
+            <input class="form-control" type="text" placeholder="Enter country" onChange={(e) => this.handleCountryName(e)} ></input>
+            <Button clickButton={() => this.getCountry(this.state.input)} children="Search"></Button>
+          </div>
+        </div>
 
+        <div className="row">
+          <div className="col-3 offset-3">
+            <Card countryImg={this.state.flag} countryName={this.state.name} countryCap={this.state.capital} countryReg={this.state.region} countryPop={this.state.population}></Card>
+          </div>
+        </div>
       </div>
 
     )
