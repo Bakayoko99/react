be7939c59d58b6fe0867167068aa6a53e2c1ee43
Bakagnan <cr7sendu@gmail.com>
2021-04-30 14:20:45 +0200

    countries bonus quasi finito

--- chapitre_05/countries/src/components/Button.jsx
+++ chapitre_05/countries/src/components/Button.jsx
@@ -5,7 +5,7 @@ class Button extends React.Component{
 
     render(){
         return(
-            <button className="btn btn-primary m-4" onClick={this.props.clickButton}>{this.props.children}</button>
+            <button className="btn btn-primary mt-3 ml-1 mb-2" onClick={this.props.clickButton}>{this.props.children}</button>
         )
 
     }
